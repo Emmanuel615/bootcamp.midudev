561b5fe27dbdde5b05fcde4ba3d78fb12da4ee29
Emmanuel Pereyra <pereyraemmanuel615@gmail.com>
2021-09-04 18:56:03 -0300

    pull en notebook personal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css'
-import Mensaje from './components/Mensaje'
-import Descripcion from './components/Descripcion'
+import "./App.css";
+import Mensaje from "./components/Mensaje";
+import Descripcion from "./components/Descripcion";
 
 function App() {
-   return (
-      <div className='App'>
-         <header className='App-header'>
-            <p>Dale caminemos</p>
-            <Mensaje color='red' message='Estamos trabajando'/>
-            <Mensaje color='green' message='En un curso'/>
-            <Mensaje color='yellow' message='De React'/>
-            <Descripcion />
-         </header>
-      </div>
-   )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Dale caminemos</p>
+                <Mensaje color="red" message="Estamos trabajando" />
+                <Mensaje color="green" message="En un curso" />
+                <Mensaje color="yellow" message="De React" />
+                <Descripcion />
+            </header>
+        </div>
+    );
 }
 
-export default App
+export default App;
