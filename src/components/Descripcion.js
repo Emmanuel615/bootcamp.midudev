87bb21fdda0d2a0d59d3083accabed8da2feec1d
Emmanuel615 <emap_91@hotmail.com>
2021-09-04 12:18:48 -0300

    primer commit del bootcamp

---
+++ src/components/Descripcion.js
@@ -0,0 +1,8 @@
+// import React from 'react'
+
+// creamos el componente descripcion
+const Descripcion = () => {
+   return <p>Esta es la app del curso fullstack bootcamp</p>
+}
+
+export default Descripcion
---
+++ src/components/Mensaje.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// creamos un componente mensaje
+// le pasamos en params un mensaje con nombre "message"
+const Mensaje = (params) => {
+   
+   //le pasamos por params un estilo css 
+   return <h1 style={{ color: params.color }}>{params.message}</h1>
+}
+
+export default Mensaje
