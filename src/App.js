55f6871d27740fe6f9330e2abbc2443c1e10573f
Emmanuel615 <emap_91@hotmail.com>
2021-09-07 17:21:29 -0300

    Se quita el ; del return de App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Mensaje from "./components/Mensaje";
 import Descripcion from "./components/Descripcion";
 
 function App() {
-<<<<<<< HEAD
    return (
       <div className='App'>
          <header className='App-header'>
@@ -15,19 +14,6 @@ function App() {
          </header>
       </div>
    )
-=======
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Dale caminemos</p>
-                <Mensaje color="red" message="Estamos trabajando" />
-                <Mensaje color="green" message="En un curso" />
-                <Mensaje color="yellow" message="De React" />
-                <Descripcion />
-            </header>
-        </div>
-    );
->>>>>>> 561b5fe27dbdde5b05fcde4ba3d78fb12da4ee29
 }
 
 export default App;
